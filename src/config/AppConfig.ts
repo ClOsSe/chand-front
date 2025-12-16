cbc23174fbd415db4265854365ba5c53ef040499
Mohammad <44271832+ClOsSe@users.noreply.github.com>
2025-12-16 14:29:38 +0330

    Update AppConfig.ts
    remove unused base url

--- src/config/AppConfig.ts
+++ src/config/AppConfig.ts
@@ -1,5 +1,5 @@
 export default class AppConfig {
-  // static baseURL = 'https://hansha.online'
+ 
   // static baseURL = 'http://127.0.0.1:8787/'
   static baseURL = 'https://chand-backend.chandforandroid.workers.dev/'
   // static baseURL = 'http://localhost:4000/'
